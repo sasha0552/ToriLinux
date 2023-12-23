a9c1654cd4ac57c4a3590d6adb73eadfc5de3523
sasha0552 <admin@sasha0552.org>
2023-12-23 05:26:04 +0000

    Bump repositories

--- airootfs/root/customize_airootfs/scripts/0100-koboldcpp-patches.sh
+++ airootfs/root/customize_airootfs/scripts/0100-koboldcpp-patches.sh
@@ -5,7 +5,4 @@ set -eu
 pushd "koboldcpp"
   # drop pstate in idle
   patch -p1 < "$CUSTOMIZE_AIROOTFS/patches/0000-koboldcpp-drop-pstate-in-idle.patch"
-
-  # faster mixtral prompt processing
-  patch -p1 < "$CUSTOMIZE_AIROOTFS/patches/0001-koboldcpp-faster-mixtral-pp.patch"
 popd
