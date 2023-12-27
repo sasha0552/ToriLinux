192866bfd45ba22f1fdce5667e20cc372b1a5e87
sasha0552 <admin@sasha0552.org>
2023-12-27 15:43:49 +0000

    Fix flash-attn building

--- airootfs/root/customize_airootfs/scripts/1000-axolotl-dependencies.sh
+++ airootfs/root/customize_airootfs/scripts/1000-axolotl-dependencies.sh
@@ -6,13 +6,26 @@ pushd "axolotl"
   # disable package caching
   export PIP_NO_CACHE_DIR=0
 
+  # limit the number of parallel jobs to avoid OOM
+  export MAX_JOBS=2
+
+  # define supported architectures
+  export TORCH_CUDA_ARCH_LIST="6.0 6.1 7.0 7.5 8.0 8.6 8.9 9.0"
+
+  # cuda home directory
+  export CUDA_HOME=/opt/cuda
+
+  # use gcc 12
+  export CC=gcc-12
+  export CXX=g++-12
+
   # create venv
   python3 -m venv venv
 
   # activate venv
   source venv/bin/activate
     # install dependencies
-    pip3 install packaging wheel
+    pip3 install packaging torch wheel
 
     # install flash-attn without build isolation
     cat requirements.txt | grep flash-attn | xargs pip3 install --no-build-isolation
