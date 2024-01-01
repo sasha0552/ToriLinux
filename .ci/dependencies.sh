685907243e61bdfd74f8f852e20e8ef2102a0bbc
sasha0552 <admin@sasha0552.org>
2024-01-01 11:56:35 +0000

    Refresh pacman keys on dependency installation

--- .ci/dependencies.sh
+++ .ci/dependencies.sh
@@ -7,5 +7,8 @@ pacman --sync --noconfirm --refresh
 # Upgrade system
 pacman --sync --noconfirm --sysupgrade
 
+# Refresh keys
+pacman-key --refresh-keys
+
 # Install required packages
 pacman --sync --noconfirm --needed archiso patch python python-jinja rdfind
