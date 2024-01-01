744a6d74e5ecfab381d998d9c4fe313f3a04c584
sasha0552 <admin@sasha0552.org>
2024-01-01 15:15:44 +0000

    Update repositories (#9)
    Misc:
    
    * Update repositories
    
    * Fix patches
    
    * Fix pacman signature checks issue

--- .ci/dependencies.sh
+++ .ci/dependencies.sh
@@ -7,5 +7,14 @@ pacman --sync --noconfirm --refresh
 # Upgrade system
 pacman --sync --noconfirm --sysupgrade
 
+# Remove existing keys
+rm -fr /etc/pacman.d/gnupg
+
+# Init keystore
+pacman-key --init
+
+# Populate keystore
+pacman-key --populate
+
 # Install required packages
 pacman --sync --noconfirm --needed archiso patch python python-jinja rdfind
