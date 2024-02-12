3976dac412fc0b545832c9abc013ed1e0ff5e5da
sasha0552 <admin@sasha0552.org>
2024-02-12 19:37:59 +0000

    Fixes

--- .ci/actions.py
+++ .ci/actions.py
@@ -3,10 +3,10 @@
 import jinja2
 
 def main():
-  for filename in [ "gh-build-iso" ]:
+  for filename in [ "gh-build-iso", "sh-build-iso" ]:
     for type in [ "cuda", "rocm" ]:
       # read input file
-      with open(f".ci/{filename}.yml.jinja2", "r") as file:
+      with open(f".ci/template/{filename}.yml.jinja2", "r") as file:
         template = jinja2.Template(file.read())
 
       # render template
@@ -14,6 +14,10 @@ def main():
 
       # FIXME: skip if hosted and rocm
       if filename == "gh-build-iso" and type == "rocm":
+        continue
+
+      # FIXME: skip if selfhosted and cuda
+      if filename == "sh-build-iso" and type == "cuda":
         continue
 
       # write output file
