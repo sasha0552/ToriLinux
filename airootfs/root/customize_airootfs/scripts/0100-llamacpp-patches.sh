9c1085e579ee326561af3fd1ef56fab0896cae80
sasha0552 <admin@sasha0552.org>
2024-04-09 04:03:02 +0000

    Refactor (#31)

--- airootfs/root/customize_airootfs/scripts/0100-llamacpp-patches.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eu
-
-# llama.cpp patches
-pushd "llama.cpp"
-  # enable prompt cache by default
-  patch -p1 < "$CUSTOMIZE_AIROOTFS/patches/0100-llamacpp-enable-prompt-cache.patch"
-popd
