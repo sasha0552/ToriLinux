477436c968dcf0b587b7e3e218a050295c06d078
sasha0552 <admin@sasha0552.org>
2024-04-09 01:41:00 +0000

    Refactor

--- airootfs/root/customize_airootfs/scripts/0000-remove-git-submodule-pointers.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -eu
-
-# remove git submodule pointers to the root project
-find -type f -name .git -exec rm {} \;
--- airootfs/root/customize_airootfs/scripts/0100-axolotl-patches.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eu
-
-# axolotl patches
-pushd "axolotl"
-  # TODO: fix xformers version
-  sed -i 's/xformers>=0.0.23/xformers==0.0.23/g' setup.py
-popd
