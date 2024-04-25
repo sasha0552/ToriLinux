d8dc050517d084217b9ab376f5dd4f8a572979e7
sasha0552 <admin@sasha0552.org>
2024-04-25 10:51:29 +0000

    Use latest release instead of latest commit on default branch for some projects

--- .ci/update.py
+++ .ci/update.py
@@ -3,25 +3,42 @@
 import configparser
 import requests
 
-def fetch_latest_revision(url):
+def fetch_latest_revision(url, strategy):
   # github
   if url.startswith("https://github.com/") and url.endswith(".git"):
     # extract repo path
     path = url[19:-4]
 
-    # get commits from api
-    response = requests.get(f"https://api.github.com/repos/{path}/commits")
+    match strategy:
+      case "commit":
+        # get commits from api
+        response = requests.get(f"https://api.github.com/repos/{path}/commits")
 
-    # throw error if not success
-    response.raise_for_status()
+        # throw error if not success
+        response.raise_for_status()
 
-    # parse json
-    data = response.json()
+        # parse json
+        data = response.json()
 
-    # return first commit id
-    return data[0]["sha"]
+        # return first commit id
+        return data[0]["sha"]
 
-  raise ValueError(f"Unsupported: {url}")
+      case "release":
+        # get releases from api
+        response = requests.get(f"https://api.github.com/repos/{path}/releases")
+
+        # throw error if not success
+        response.raise_for_status()
+
+        # parse json
+        data = response.json()
+
+        # find first release
+        for release in data:
+          if not release["prerelease"]:
+            return release["tag_name"]
+
+  raise ValueError(f"Unsupported url or strategy ({url}, {strategy})")
 
 def main():
   # load options
@@ -29,11 +46,25 @@ def main():
   config.read(".ci/options.ini")
 
   # default option value
-  config["revisions"] = {}
+  if "revisions" not in config:
+    config["revisions"] = {}
+
+  # default option value
+  if "strategy" not in config:
+    config["strategy"] = {}
 
   # iterate over repositories
   for key, value in config.items("repositories"):
-    config["revisions"][key] = fetch_latest_revision(value)
+    # update strategy
+    strategy = "commit"
+
+    # extract strategy
+    if key in config["strategy"]:
+      strategy = config["strategy"][key]
+
+    # if not locked, update
+    if strategy != "locked":
+      config["revisions"][key] = fetch_latest_revision(value, strategy)
 
   # write config back
   with open(".ci/options.ini", "w") as file:
